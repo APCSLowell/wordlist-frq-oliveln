1e15e4d75026ba16167acc07c8da35fc94e45548
oliveln <143148891+oliveln@users.noreply.github.com>
2025-04-08 10:51:39 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -23,7 +23,7 @@ public class WordList
   {
     int counter = 0;
     for(int i = myList.size() - 1; i >= 0; i--){
-      if(myList.get(i).length == len){
+      if(myList.get(i).length() == len){
         myList.remove(i);
       }
     }
